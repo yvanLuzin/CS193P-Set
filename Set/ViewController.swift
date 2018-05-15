d4216cdca94441e6f909d57faace3789ae64ea56
Иван Лузин <ivan.luzin@icloud.com>
2018-05-15 10:52:09 +0500

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Иван Лузин on 15.05.2018.
+//  Copyright © 2018 Иван Лузин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
